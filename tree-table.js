a8aa1d3b7492a38585054f4dbe1180eeca0b3fef
lisi01 <lisi01@360.net>
2019-08-06 17:18:44 +0800

    fix: hide children when father root is close

--- tree-table.js
+++ tree-table.js
@@ -207,15 +207,18 @@
                 if (contain && contain === curr[curr.length - 1] && Number(trLevel) > (Number(level))) {
                     var span = $(tempTr.children()[0].children[Number(trLevel)]);
                     var isOpen = $(span).attr('data-is-open');
+                    var childrenCount = $(span).attr('data-count');
                     tempTr.removeClass('hidden');
                     tempTr.addClass('show');
-                    if (isOpen && isOpen === '0') { // 下级折叠状态
-                        $(span).attr('data-is-open', '1');
-                        $(span).text('-');
+                    // pLevel != -1 并且有子级的情况下，判断pLevel的开关状态，关闭则不展开其下级元素
+                    if (isOpen && isOpen === '0' && Number(childrenCount) > 0) { // 下级折叠状态
+                        i = i + Number(childrenCount);
                     } else {
                       if (isOpen === '1') {
-                        // $(span).attr('data-is-open', '1');
-                        // $(span).text('-');
+                        $(span).attr('data-is-open', '1');
+                        $(span).text('-');
+                        tempTr.removeClass('hidden');
+                        tempTr.addClass('show');
                       }
                     }
                 }
@@ -224,6 +227,26 @@
             $(this).attr('data-is-open', '1');
         }
     });
+
+    function countChildren(node) {
+        var sum = 0,
+          children = node && node.length ? node : node.children,
+          i = children && children.length;
+
+        if (!i) {
+            sum = 0;
+        } else {
+            while (--i >= 0) {
+                if (node && node.length) {
+                    sum++;
+                    countChildren(children[i]);
+                } else {
+                    sum += countChildren(children[i]);
+                }
+            }
+        }
+        return sum;
+    }
 
     function createRows() {
         var fragments = document.createDocumentFragment();
@@ -234,6 +257,7 @@
             $(trEle).attr('data-part-level', item.partLevel);
             $(trEle).attr('data-all-level', item.allLevel);
             $(trEle).attr('data-level', item.level);
+            $(trEle).attr('data-count', countChildren(item));
             var tdEle1 = document.createElement('td');
             for (var j =0; j <= item.level; j++) {
                 var spanEle = document.createElement('span');
@@ -246,6 +270,7 @@
                     if (item.children && item.children.length > 0) {
                         $(spanEle).addClass('btn-toggle expand');
                         $(spanEle).attr('data-is-open', '1');
+                        $(spanEle).attr('data-count', countChildren(item));
                         $(spanEle).text('-');
                     } else {
                         $(spanEle).addClass('last-block');
